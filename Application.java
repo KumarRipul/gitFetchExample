8bbac8390f4e0b8325452d7a70bec6e7bb928992
Ripul <ripul.ku@gmail.com>
2021-05-23 03:45:06 +0200

    Initial Commit

---
+++ Application.java
@@ -0,0 +1,39 @@
+package za.co.mq.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author akhand
+ */
+
+@SpringBootApplication(scanBasePackages = {"za.co.mq.commons.config", "za.co.mq.aws","za.co.mq.audit"})
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("POST", "GET", "PUT", "DELETE");
+            }
+        };
+    }
+
+}
